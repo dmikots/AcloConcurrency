665ac03eaf2a50c2016f5791ce88e0972db50ca7
dmikots <57399131+dmikots@users.noreply.github.com>
2022-06-11 21:20:09 +0300

    init

---
+++ AcloConcurrency/AcloConcurrency/AcloConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  AcloConcurrencyApp.swift
+//  AcloConcurrency
+//
+//  Created by DariaMikots on 11.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct AcloConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
