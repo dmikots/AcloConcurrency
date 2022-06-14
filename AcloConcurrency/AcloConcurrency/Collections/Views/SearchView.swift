401500fd2ee26173316381197b68b604a921b6e9
dmikots <57399131+dmikots@users.noreply.github.com>
2022-06-14 23:40:41 +0300

    done tabView + 2 default views

---
+++ AcloConcurrency/AcloConcurrency/Collections/Views/SearchView.swift
@@ -0,0 +1,22 @@
+//
+//  SearchView.swift
+//  AlcoConcurrency
+//
+//  Created by DariaMikots on 14.06.2022.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        NavigationView{
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        }
+    }
+}
+
+struct SearchView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchView()
+    }
+}
